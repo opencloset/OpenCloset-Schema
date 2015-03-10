98f4f474c7f9427b7fac35e57c13ca27d1714b4a
Keedi Kim <keedi.k@gmail.com>
2015-03-10 16:37:52 +0900

    After release: bump $VERSION and timestamp Changes

--- lib/OpenCloset/Schema/Result/Group.pm
+++ lib/OpenCloset/Schema/Result/Group.pm
@@ -83,7 +83,7 @@ __PACKAGE__->has_many(
 
 # ABSTRACT: OpenCloset Database Schema Class
 
-our $VERSION = '0.010';
+our $VERSION = '0.011';
 
 1;
 
--- lib/OpenCloset/Schema/Result/Order.pm
+++ lib/OpenCloset/Schema/Result/Order.pm
@@ -561,7 +561,7 @@ __PACKAGE__->belongs_to(
 
 # ABSTRACT: OpenCloset Database Schema Class
 
-our $VERSION = '0.010';
+our $VERSION = '0.011';
 
 =head2 clothes
 
