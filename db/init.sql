0abaea2e6d07e464b2336583647774974c670ed8
Hyungsuk Hong <aanoaa@gmail.com>
2016-01-13 15:51:49 +0900

    Add `suit` table to init.sql

--- db/init.sql
+++ db/init.sql
@@ -528,3 +528,18 @@ CREATE TABLE `volunteer_guestbook` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_volunteer_guestbook1` FOREIGN KEY (`volunteer_work_id`) REFERENCES `volunteer_work` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- suit
+--
+
+CREATE TABLE `suit` (
+  `code_top`    CHAR(5) NOT NULL,
+  `code_bottom` CHAR(5) NOT NULL,
+
+  PRIMARY KEY (`code_top`),
+  UNIQUE KEY  (`code_bottom`),
+
+  CONSTRAINT `fk_suit1` FOREIGN KEY (`code_top`)    REFERENCES `clothes` (`code`) ON DELETE CASCADE,
+  CONSTRAINT `fk_suit2` FOREIGN KEY (`code_bottom`) REFERENCES `clothes` (`code`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
