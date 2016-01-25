9ff93a61207a307930b95b7b7e2e4333234aed04
Hyungsuk Hong <aanoaa@gmail.com>
2016-01-25 18:37:37 +0900

    Add db/alter/donation-form.sql

---
+++ db/alter/donation-form.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `donation_form` ;
+CREATE TABLE `donation_form` (
+  `id`              INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`            VARCHAR(32)  DEFAULT NULL,
+  `ever_sent`       INT(11)      DEFAULT NULL COMMENT '0 is false, otherwise true',
+  `gender`          INT(11)      DEFAULT NULL COMMENT '1: male, 2: female',
+  `phone`           CHAR(11)     DEFAULT NULL COMMENT 'format: 010xxxxxxxx',
+  `email`           VARCHAR(128) DEFAULT NULL,
+  `address`         TEXT         DEFAULT NULL,
+  `category`        VARCHAR(128) DEFAULT NULL,
+  `quantity`        VARCHAR(32)  DEFAULT NULL,
+  `terms`           INT(11)      DEFAULT NULL COMMENT '0 is false, otherwise true',
+  `talent_donation` INT(11)      DEFAULT NULL COMMENT '0 is false, otherwise true',
+  `talent`          TEXT         DEFAULT NULL,
+
+  --                                                    접수 | 발송 | 반송 | 완료 | 취소
+  `status`          VARCHAR(32)  DEFAULT NULL COMMENT 'accept | sent | return | done | cancel',
+  `created_date`    DATETIME     DEFAULT NULL,
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
