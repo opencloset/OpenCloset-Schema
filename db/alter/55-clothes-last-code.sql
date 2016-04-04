5e975c7dfb91dc7fdeaa59a6dea5f977b84b390b
Hyungsuk Hong <aanoaa@gmail.com>
2016-04-04 17:55:32 +0900

    Insert last code

--- db/alter/55-clothes-last-code.sql
+++ db/alter/55-clothes-last-code.sql
@@ -4,3 +4,17 @@ CREATE TABLE `clothes_code` (
   `code`     CHAR(5)     NOT NULL,
   PRIMARY KEY (`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `clothes_code` (`category`, `code`) VALUES
+  ("shoes", "0A18F"),
+  ("blouse", "0B0T5"),
+  ("coat", "0C04U"),
+  ("belt", "0E149"),
+  ("hat", "0H00O"),
+  ("jacket", "0J2JN"),
+  ("skirt", "0K12V"),
+  ("onepiece", "0O04U"),
+  ("pants", "0P1VG"),
+  ("shirt", "0S2EX"),
+  ("tie", "0T1DA"),
+  ("waistcoat", "0W01D");
