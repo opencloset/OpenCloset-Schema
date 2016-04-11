be602f7e55b8c0ac805280c3390c8bffff6986c4
Yongbin Yu <yongbin.yu@gmail.com>
2016-04-11 15:45:45 +0900

    Add logging related column and change trigger body
    clothes의 cuff 항목이 추가됨에 따라서 clothes의 변경을 저장하는
    order_size_log에 해당 컬럼을 추가하고 order_detail을 통한 트리거도
    적절하게 수정했습니다.

--- db/alter/57-clothes-cuff.sql
+++ db/alter/57-clothes-cuff.sql
@@ -1,2 +1,36 @@
 ALTER TABLE `clothes` ADD COLUMN `cuff` INT DEFAULT 0 AFTER `length`;
 ALTER TABLE `clothes` ADD INDEX (`cuff`);
+ALTER TABLE `order_size_log` ADD `cuff` INT(11)  NULL  DEFAULT '0'  AFTER `length`;
+
+DROP TRIGGER IF EXISTS `tracking_order_clothes_size_insert`;
+
+DELIMITER $$
+CREATE TRIGGER `tracking_order_clothes_size_insert` AFTER INSERT ON `order_detail` FOR EACH ROW
+INSERT INTO `order_size_log`
+(
+    SELECT
+            a.order_id
+            ,c.id
+            ,a.clothes_code
+            ,now()
+            ,b.category
+            ,b.color
+            ,b.gender
+            ,b.neck
+            ,b.bust
+            ,b.waist
+            ,b.hip
+            ,b.topbelly
+            ,b.belly
+            ,b.arm
+            ,b.thigh
+            ,b.length
+            ,b.cuff
+            ,b.price
+    FROM `order_detail` AS a
+      INNER JOIN `clothes` AS b ON a.`clothes_code` = b.`code`
+      INNER JOIN `order`   AS c ON a.`order_id` = c.`id`
+    WHERE a.order_id = NEW.order_id AND a.clothes_code = NEW.clothes_code
+    LIMIT 1
+) $$
+DELIMITER ;
