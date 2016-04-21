2af0eda1ba01c89d59f130e3f52102103c911387
Yongbin Yu <yongbin.yu@gmail.com>
2016-04-21 13:47:38 +0900

    Revert incorrect cuff default values to null
    새로운 컬럼이 추가된 경우 DEFAULT값이 모두 적용됩니다. 현재 기존
    스키마에서 사이즈 컬럼의 DEFAULT값이 0으로 지정되어있기는 하지만
    응용프로그램단에서 NULL을 전달하기때문에 사용자가 입력하지 않은 값은
    NULL이 들어있는것이 일관성이 있기때문에 새롭게 추가된 cuff 컬럼도 NULL로
    지정합니다.(2016-04-21 IRC 로그 참조)

--- db/alter/59-decimal-cuff.sql
+++ db/alter/59-decimal-cuff.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `clothes`        MODIFY `cuff` DECIMAL(5, 2) DEFAULT 0.00;
 ALTER TABLE `order_size_log` MODIFY `cuff` DECIMAL(5, 2) DEFAULT 0.00;
+
+UPDATE `clothes`        SET `cuff` = NULL WHERE `cuff` = 0.00;
+UPDATE `order_size_log` SET `cuff` = NULL WHERE `cuff` = 0.00;
