c08cf0f0666badc9d1f767361a04de427fffab87
Hyungsuk Hong <aanoaa@gmail.com>
2016-05-18 13:54:34 +0900

    Add Abstract, Version and Copyright to DiscardClothes

--- lib/OpenCloset/Schema/Result/DiscardClothes.pm
+++ lib/OpenCloset/Schema/Result/DiscardClothes.pm
@@ -157,4 +157,13 @@ __PACKAGE__->belongs_to(
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+# ABSTRACT: OpenCloset Database Schema Class
+
+our $VERSION = '0.034';
+
 1;
+
+# COPYRIGHT
+
+__END__
