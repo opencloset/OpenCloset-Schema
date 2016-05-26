93f45ed6322d3eb9edd20ef0cb89298e60a15749
Hyungsuk Hong <aanoaa@gmail.com>
2016-05-26 18:06:08 +0900

    After release: bump $VERSION and timestamp Changes

--- lib/OpenCloset/Schema/Result/Clothes.pm
+++ lib/OpenCloset/Schema/Result/Clothes.pm
@@ -464,7 +464,7 @@ __PACKAGE__->many_to_many( "tags", "clothes_tags", "tag" );
 
 # ABSTRACT: OpenCloset Database Schema Class
 
-our $VERSION = '0.035';
+our $VERSION = '0.036';
 
 =head2 orders
 
--- lib/OpenCloset/Schema/Result/OrderSizeLog.pm
+++ lib/OpenCloset/Schema/Result/OrderSizeLog.pm
@@ -204,6 +204,6 @@ __PACKAGE__->add_columns(
 
 # ABSTRACT: OpenCloset Database Schema Class
 
-our $VERSION = '0.035';
+our $VERSION = '0.036';
 
 1;
