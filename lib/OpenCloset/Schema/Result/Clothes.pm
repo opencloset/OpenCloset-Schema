10a2ba02d741efc3c52e71e93e5004aa71559996
Keedi Kim <keedi.k@gmail.com>
2016-05-30 18:06:50 +0900

    clone() must be called to prevent original datetime is changed

--- lib/OpenCloset/Schema/Result/Clothes.pm
+++ lib/OpenCloset/Schema/Result/Clothes.pm
@@ -517,8 +517,8 @@ sub rented_duration {
 
     my $sum = 0;
     for my $order_detail (@order_details) {
-        my $rental_date = $order_detail->order->rental_date->truncate( to => 'day' );
-        my $return_date = $order_detail->order->return_date->truncate( to => 'day' );
+        my $rental_date = $order_detail->order->rental_date->clone->truncate( to => 'day' );
+        my $return_date = $order_detail->order->return_date->clone->truncate( to => 'day' );
 
         next unless $rental_date <= $return_date;
 
