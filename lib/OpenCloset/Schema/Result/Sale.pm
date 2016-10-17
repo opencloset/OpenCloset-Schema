2ff78d9cc8726394f76010a731181dc61ca9aaeb
Keedi Kim <keedi.k@gmail.com>
2016-10-17 17:07:57 +0900

    Add & update dbicdump auto-generated lib files

---
+++ lib/OpenCloset/Schema/Result/Sale.pm
@@ -0,0 +1,149 @@
+#<<<
+use utf8;
+
+package OpenCloset::Schema::Result::Sale;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+OpenCloset::Schema::Result::Sale
+
+=cut
+
+use strict;
+use warnings;
+
+=head1 BASE CLASS: L<OpenCloset::Schema::Base>
+
+=cut
+
+use base 'OpenCloset::Schema::Base';
+
+=head1 TABLE: C<sale>
+
+=cut
+
+__PACKAGE__->table("sale");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 name
+
+  data_type: 'char'
+  is_nullable: 0
+  size: 32
+
+=head2 desc
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 create_date
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  inflate_datetime: 1
+  is_nullable: 1
+  set_on_create: 1
+
+=head2 update_date
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  inflate_datetime: 1
+  is_nullable: 1
+  set_on_create: 1
+  set_on_update: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+    "id",
+    {
+        data_type         => "integer",
+        extra             => { unsigned => 1 },
+        is_auto_increment => 1,
+        is_nullable       => 0,
+    },
+    "name",
+    { data_type => "char", is_nullable => 0, size => 32 },
+    "desc",
+    { data_type => "text", is_nullable => 1 },
+    "create_date",
+    {
+        data_type                 => "datetime",
+        datetime_undef_if_invalid => 1,
+        inflate_datetime          => 1,
+        is_nullable               => 1,
+        set_on_create             => 1,
+    },
+    "update_date",
+    {
+        data_type                 => "datetime",
+        datetime_undef_if_invalid => 1,
+        inflate_datetime          => 1,
+        is_nullable               => 1,
+        set_on_create             => 1,
+        set_on_update             => 1,
+    },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<name>
+
+=over 4
+
+=item * L</name>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint( "name", ["name"] );
+
+=head1 RELATIONS
+
+=head2 order_sales
+
+Type: has_many
+
+Related object: L<OpenCloset::Schema::Result::OrderSale>
+
+=cut
+
+__PACKAGE__->has_many(
+    "order_sales", "OpenCloset::Schema::Result::OrderSale",
+    { "foreign.sale_id" => "self.id" }, { cascade_copy => 0, cascade_delete => 0 },
+);
+
+#>>>
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2016-10-17 16:59:41
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:S69JNXO5nuGGiDS+4Xmgkg
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
