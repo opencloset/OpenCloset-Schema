b98850cf30decfa977d80bcd2e4910321601fe75
Hyungsuk Hong <aanoaa@gmail.com>
2017-04-03 12:13:56 +0900

    Add more columns to visitor for 취업날개

--- db/alter/120-stat-visitor.sql
+++ db/alter/120-stat-visitor.sql
@@ -10,12 +10,21 @@ CREATE TABLE `visitor` (
   `reserved`         INT DEFAULT 0,
   `reserved_male`    INT DEFAULT 0,
   `reserved_female`  INT DEFAULT 0,
+
   `visited`          INT DEFAULT 0,
   `visited_male`     INT DEFAULT 0,
   `visited_female`   INT DEFAULT 0,
+  `visited_age_10`   INT DEFAULT 0,
+  `visited_age_20`   INT DEFAULT 0,
+  `visited_age_30`   INT DEFAULT 0,
+
   `unvisited`        INT DEFAULT 0,
   `unvisited_male`   INT DEFAULT 0,
   `unvisited_female` INT DEFAULT 0,
+  `unvisited_age_10` INT DEFAULT 0,
+  `unvisited_age_20` INT DEFAULT 0,
+  `unvisited_age_30` INT DEFAULT 0,
+
   `rented`           INT DEFAULT 0,
   `rented_male`      INT DEFAULT 0,
   `rented_female`    INT DEFAULT 0,
