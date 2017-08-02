7bba1715fcdc994e3d77fde77fabf29a25966f7c
Hyungsuk Hong <aanoaa@gmail.com>
2017-08-02 23:10:18 +0900

    Add `order`.`agent` column and `order_agent` table

--- db/init.sql
+++ db/init.sql
@@ -419,6 +419,7 @@ CREATE TABLE `order` (
   `bestfit`               BOOLEAN DEFAULT 0,
   `ignore`                INT DEFAULT NULL COMMENT 'null and 0 are false, otherwise true',
   `ignore_sms`            INT DEFAULT NULL COMMENT 'null and 0 are false, otherwise true',
+  `agent`                 INT DEFAULT NULL COMMENT 'null and 0 are false, otherwise true',
 
   `create_date`           DATETIME DEFAULT NULL,
   `update_date`           DATETIME DEFAULT NULL,
