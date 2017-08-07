e4b2f13e804901c42ee43ec6fe88300f7bc6bf58
Hyungsuk Hong <aanoaa@gmail.com>
2017-08-07 14:07:03 +0900

    Add order_agent table schema to init.sql

--- db/init.sql
+++ db/init.sql
@@ -941,3 +941,34 @@ CREATE TABLE `visitor` (
 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- order_agent
+--
+DROP TABLE IF EXISTS `order_agent`;
+CREATE TABLE `order_agent` (
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `order_id`      INT UNSIGNED NOT NULL,
+
+  `label`         VARCHAR(128) DEFAULT NULL,
+  `gender`        VARCHAR(6)   DEFAULT NULL COMMENT 'male/female',
+
+  `height`        INT DEFAULT NULL,
+  `weight`        INT DEFAULT NULL,
+  `neck`          INT DEFAULT NULL,
+  `bust`          INT DEFAULT NULL,
+  `waist`         INT DEFAULT NULL,
+  `hip`           INT DEFAULT NULL,
+  `topbelly`      INT DEFAULT NULL,
+  `belly`         INT DEFAULT NULL,
+  `thigh`         INT DEFAULT NULL,
+  `arm`           INT DEFAULT NULL,
+  `leg`           INT DEFAULT NULL,
+  `knee`          INT DEFAULT NULL,
+  `foot`          INT DEFAULT NULL,
+  `pants`         INT DEFAULT NULL,
+  `skirt`         INT DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_order_agent1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
