3a8faca7734ee742f33a8a275491d55df73ca0dd
Hyungsuk Hong <aanoaa@gmail.com>
2018-12-15 02:09:17 +0900

    Alter `event` table and add `event_type` table

---
+++ db/alter/142-event-type.sql
@@ -0,0 +1,33 @@
+SET NAMES utf8;
+DROP TABLE IF EXISTS `event_type`;
+CREATE TABLE `event_type` (
+  `id`           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `type`         VARCHAR(128) NOT NULL COMMENT 'START_TYPE(reserve or rental)|END_TYPE(reserve or rental)',
+  `domain`       VARCHAR(128) NOT NULL COMMENT 'rental or donation or volunteer',
+  `desc`         TEXT NOT NULL,
+  `create_date`  DATETIME DEFAULT NULL,
+  `update_date`  DATETIME DEFAULT NULL,
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `event_type` (`type`, `domain`, `desc`, `create_date`, `update_date`)
+  VALUES
+  ('reserve|reserve' , 'rental', '예약하는 날짜부터 이벤트 시작, 마감날짜 = 예약하는날짜', NOW(), NOW()),
+  ('reserve|rental'  , 'rental', '예약하는 날짜부터 이벤트 시작, 마감날짜 = 대여일', NOW(), NOW()),
+  ('rental|reserve'  , 'rental', '대여일부터 이벤트 시작, 마감날짜 = 예약하는날짜', NOW(), NOW()),
+  ('rental|rental'   , 'rental', '대여일부터 이벤트 시작, 마감날짜 = 대여일', NOW(), NOW());
+
+ALTER TABLE `event`
+  ADD COLUMN `event_type_id` INT UNSIGNED
+  DEFAULT NULL AFTER `id`;
+
+ALTER TABLE `event`
+  ADD COLUMN `free_shipping` INT
+  DEFAULT 0 AFTER `nth`;
+
+ALTER TABLE `event`
+  ADD CONSTRAINT `fk_event1`
+  FOREIGN KEY (`event_type_id`)
+  REFERENCES `event_type` (`id`)
+  ON DELETE CASCADE;
--- db/init.sql
+++ db/init.sql
@@ -631,22 +631,39 @@ CREATE TABLE `suit` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- event
+-- event_type
 --
-DROP TABLE IF EXISTS `event`;
-CREATE TABLE `event` (
+DROP TABLE IF EXISTS `event_type`;
+CREATE TABLE `event_type` (
   `id`           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name`         VARCHAR(32) NOT NULL,
-  `desc`         TEXT DEFAULT NULL,
-  `sponsor`      VARCHAR(128) DEFAULT NULL,
-  `year`         INT(11) DEFAULT 0 COMMENT '연도; 애매하면 0',
-  `nth`          INT(11) DEFAULT 1 COMMENT '회차; 회차와 연도로 그룹화; 2018년 2회차; 2019년 1회차',
-  `start_date`   DATETIME DEFAULT NULL,
-  `end_date`     DATETIME DEFAULT NULL,
+  `type`         VARCHAR(128) NOT NULL COMMENT 'START_TYPE(reserve or rental)|END_TYPE(reserve or rental)',
+  `domain`       VARCHAR(128) NOT NULL COMMENT 'rental or donation or volunteer',
+  `desc`         TEXT NOT NULL,
   `create_date`  DATETIME DEFAULT NULL,
   `update_date`  DATETIME DEFAULT NULL,
 
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- event
+--
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE `event` (
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `event_type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`          VARCHAR(32) NOT NULL,
+  `desc`          TEXT DEFAULT NULL,
+  `sponsor`       VARCHAR(128) DEFAULT NULL,
+  `year`          INT(11) DEFAULT 0 COMMENT '연도; 애매하면 0',
+  `nth`           INT(11) DEFAULT 1 COMMENT '회차; 회차와 연도로 그룹화; 2018년 2회차; 2019년 1회차',
+  `start_date`    DATETIME DEFAULT NULL,
+  `end_date`      DATETIME DEFAULT NULL,
+  `create_date`   DATETIME DEFAULT NULL,
+  `update_date`   DATETIME DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_event1` FOREIGN KEY (`event_type_id`) REFERENCES `event_type` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
